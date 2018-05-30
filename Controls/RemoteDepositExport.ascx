2a4de8ac08fc7b69d42047330e7f4d3c1fbe6243
Daniel Hazelbaker <daniel@blueboxmoon.com>
2018-05-30 11:25:08 -0700

    - Added temporary testing block for exporting a single batch for remote deposit.

---
+++ Controls/RemoteDepositExport.ascx
@@ -0,0 +1,22 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="RemoteDepositExport.ascx.cs" Inherits="RockWeb.Plugins.com_shepherdchurch.ImageCashLetter.RemoteDepositExport" %>
+
+<asp:UpdatePanel ID="upnlContent" runat="server">
+    <ContentTemplate>
+        <Rock:NotificationBox ID="nbWarningMessage" runat="server" NotificationBoxType="Danger" Visible="true" />
+
+        <Rock:RockDropDownList ID="ddlFileFormat" runat="server" Label="File Format" Required="true" />
+
+        <Rock:RockTextBox ID="tbBatchId" runat="server" Label="Batch ID" Required="true" />
+
+        <div class="actions margin-t-md">
+            <asp:LinkButton ID="lbExport" runat="server" Text="Export" CssClass="btn btn-primary" OnClick="lbExport_Click" />
+        </div>
+
+        <asp:Panel ID="pnlSuccess" runat="server" Visible="false" CssClass="alert alert-success margin-t-lg">
+            <p>Data has been successfully exported.</p>
+            <p>
+                <asp:HyperLink ID="hlDownload" runat="server" Text="Download" CssClass="btn btn-success" />
+            </p>
+        </asp:Panel>
+    </ContentTemplate>
+</asp:UpdatePanel>
---
+++ Controls/RemoteDepositExport.ascx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.UI.WebControls;
+
+using Rock;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.UI;
+
+using com.shepherdchurch.ImageCashLetter;
+using com.shepherdchurch.ImageCashLetter.Model;
+using System.ComponentModel;
+
+namespace RockWeb.Plugins.com_shepherdchurch.ImageCashLetter
+{
+    [DisplayName( "Remote Deposit Export" )]
+    [Category( "Shepherd Church > Image Cash Letter" )]
+    [Description( "Exports batch data for use remote deposit with a bank." )]
+    public partial class RemoteDepositExport : RockBlock
+    {
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad( e );
+
+            if ( !IsPostBack )
+            {
+                var fileFormatService = new ImageCashLetterFileFormatService( new RockContext() );
+                var fileFormats = fileFormatService.Queryable().Where( f => f.IsActive == true );
+
+                ddlFileFormat.Items.Clear();
+                ddlFileFormat.Items.Add( new ListItem() );
+                foreach ( var fileFormat in fileFormats )
+                {
+                    ddlFileFormat.Items.Add( new ListItem( fileFormat.Name, fileFormat.Id.ToString() ) );
+                }
+            }
+        }
+
+        protected void lbExport_Click( object sender, EventArgs e )
+        {
+            using ( var rockContext = new RockContext() )
+            {
+                var batchId = tbBatchId.Text.AsInteger();
+                var batches = new FinancialBatchService( rockContext ).Queryable().Where( b => b.Id == batchId ).ToList();
+                var fileFormat = new ImageCashLetterFileFormatService( rockContext ).Get( ddlFileFormat.SelectedValue.AsInteger() );
+                var component = FileFormatTypeContainer.GetComponent( fileFormat.EntityType.Name );
+                List<string> errorMessages;
+
+                fileFormat.LoadAttributes( rockContext );
+
+                var mergeFields = new Dictionary<string, object>
+                {
+                    {  "FileFormat", fileFormat }
+                };
+                var filename = fileFormat.FileNameTemplate.ResolveMergeFields( mergeFields );
+
+                var stream = component.ExportBatches( fileFormat, batches, out errorMessages );
+
+                var binaryFileService = new BinaryFileService( rockContext );
+                var binaryFileTypeService = new BinaryFileTypeService( rockContext );
+                var binaryFile = new BinaryFile
+                {
+                    BinaryFileTypeId = binaryFileTypeService.Get( Rock.SystemGuid.BinaryFiletype.DEFAULT.AsGuid() ).Id,
+                    IsTemporary = true,
+                    FileName = filename,
+                    MimeType = "octet/stream",
+                    ContentStream = stream
+                };
+
+                binaryFileService.Add( binaryFile );
+                rockContext.SaveChanges();
+
+                pnlSuccess.Visible = true;
+                hlDownload.NavigateUrl = ResolveUrl( string.Format( "~/GetFile.ashx?Id={0}&attachment=True", binaryFile.Id ) );
+            }
+        }
+    }
+}
